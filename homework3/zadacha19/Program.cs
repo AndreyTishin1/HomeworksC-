00e2de26b68b29fc5b03264c1b06dd98136d4342
Andrey <atishin1999@mail.ru>
2023-06-23 18:01:49 +0300

    palindrom

---
+++ homework3/zadacha19/Program.cs
@@ -0,0 +1,31 @@
+﻿/*Задача 19
+Напишите программу, которая принимает на вход пятизначное число и проверяет, является ли оно палиндромом.
+14212 -> нет
+12821 -> да
+23432 -> да*/
+
+void palindrom(int number)
+{
+    int a = 0;
+    int summa = 0;
+    for (a = number; number != 0; number = number / 10)
+    {
+        int b = number % 10;
+        summa = summa * 10 + b;
+    }
+    if (a == summa)
+        Console.WriteLine($"Число {a} является палиндромом.");
+    else
+        Console.WriteLine($"Число {a} не является палиндромом");
+}
+
+Console.WriteLine("Введите пятизначное число: ");
+int number = int.Parse(Console.ReadLine());
+if (number < 10000 || number > 99999)
+{
+    Console.WriteLine("Число не пятизначное");
+}
+else
+{
+    palindrom(number);
+}
