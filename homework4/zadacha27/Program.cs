b01e68e79ecae790e71b7ae7df4ced4281b55d02
Andrey <atishin1999@mail.ru>
2023-06-25 02:21:49 +0300

    zadacha27

---
+++ homework4/zadacha27/Program.cs
@@ -0,0 +1,16 @@
+﻿// Задача 27: Напишите программу, которая принимает на вход число и выдаёт сумму цифр в числе.
+
+void summa(int number)
+{
+    int index = number;
+    int summ = 0;
+    while (index > 0)
+    {
+        summ = summ + (index % 10);
+        index = index / 10;
+    }
+    Console.WriteLine($"Сумма цифр числа {number} равна {summ}");
+}
+Console.WriteLine("Введите число: ");
+int number = Math.Abs(int.Parse(Console.ReadLine()));
+summa(number);
