4de590335fbafc8aef3b4c7a2f3528b2c5db80bc
Andrey <atishin1999@mail.ru>
2023-06-25 03:06:27 +0300

    zadacha29

---
+++ homework4/zadacha29/Program.cs
@@ -0,0 +1,19 @@
+﻿// Задача 29: Напишите программу, которая задаёт массив из 8 элементов и выводит их на экран.
+
+void massiv(int number)
+{
+    int[] array = new int[number];
+    for (int i = 0; i < array.Length; i++)
+    {
+        array[i]= new Random().Next(0,15);
+    }
+
+    for (int i = 0; i < array.Length; i++)
+    {
+        Console.Write($"{array[i]} ");
+    }
+}
+
+Console.Write("Введите размер массива: ");
+int number = int.Parse(Console.ReadLine());
+massiv(number);
