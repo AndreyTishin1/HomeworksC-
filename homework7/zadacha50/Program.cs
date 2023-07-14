4dda7a5eea39c56ca184c1340fc21ca7352de13d
Andrey <atishin1999@mail.ru>
2023-07-14 16:51:09 +0300

    zadacha50

---
+++ homework7/zadacha50/Program.cs
@@ -0,0 +1,45 @@
+﻿/*Задача 50. Напишите программу, которая на вход принимает позиции элемента в двумерном массиве, и возвращает значение этого элемента или же указание, что такого элемента нет.
+
+Например, задан массив:
+1 4 7 2
+5 9 2 3
+8 4 2 4
+17 -> такого числа в массиве нет
+*/
+
+Console.WriteLine("Введите количество строк");
+int m = int.Parse(Console.ReadLine());
+Console.WriteLine("Введите количество столбцов");
+int n = int.Parse(Console.ReadLine());
+
+double[,] array = new double[m, n];
+
+for (int i = 0; i < m; i++)
+{
+    for (int j = 0; j < n; j++)
+    {
+        array[i, j] = Math.Round((new Random().NextDouble() * 20), 2);
+        Console.Write(array[i, j] + " ");
+    }
+    Console.WriteLine();
+}
+
+Console.WriteLine("Введите номер строки элемента:");
+int str = int.Parse(Console.ReadLine());
+Console.WriteLine("Введите столбец элемента");
+int stolb = int.Parse(Console.ReadLine());
+
+for (int i = 0; i < m; i++)
+{
+    for (int j = 0; j < n; j++)
+    {
+        if (str == i && stolb == j)
+        {
+            Console.WriteLine(array[i, j]);
+            break;
+        }
+
+    }
+    Console.WriteLine("элемента с таким индексом нет в массиве.");
+}
+
