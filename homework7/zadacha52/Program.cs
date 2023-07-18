c13e8405e4bdfe9fde00df1faaecff3daac33ba4
Andrey <atishin1999@mail.ru>
2023-07-18 17:10:42 +0300

    zadacha54

--- homework7/zadacha52/Program.cs
+++ homework7/zadacha52/Program.cs
@@ -5,7 +5,7 @@
 5 9 2 3
 8 4 2 4
 Среднее арифметическое каждого столбца: 4,6; 5,6; 3,6; 3.
-*/
+
 
 Console.WriteLine("Введите количество строк");
 int m = int.Parse(Console.ReadLine());
@@ -34,3 +34,24 @@ for (int i = 0; i < array.GetLength(1); i++)
     Console.Write($"Среднее арифметическое столбца {n} равно {sum/array.GetLength(1)} " );
     Console.WriteLine();
 }
+*/
+
+Console.Write("Введите размерность m массива: ");
+int m = int.Parse(Console.ReadLine());
+Console.Write("Введите размерность n массива: ");
+int n = int.Parse(Console.ReadLine());
+int[,] array = new int[m, n];
+void massiv(int m, int n)
+{
+
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        for (int j = 0; j < array.GetLength(1); j++)
+        {
+            array[i, j] = new Random().Next(1,9);
+            Console.Write(array[i, j] + " ");
+        }
+    Console.WriteLine();    
+    }
+}
+massiv(m, n);
---
+++ homework8/zadacha54/Program.cs
@@ -0,0 +1,67 @@
+﻿Console.Write("Введите размерность m массива: ");
+int m = int.Parse(Console.ReadLine());
+Console.Write("Введите размерность n массива: ");
+int n = int.Parse(Console.ReadLine());
+int[,] array = new int[m, n];
+
+void massiv(int m, int n)
+{
+
+    for (int i = 0; i < m; i++)
+    {
+        for (int j = 0; j < n; j++)
+        {
+            array[i, j] = new Random().Next(1, 9);
+            Console.Write(array[i, j] + " ");
+        }
+        Console.WriteLine();
+    }
+}
+
+void printm(int[,] array)
+{
+
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        for (int j = 0; j < array.GetLength(1); j++)
+        {
+            Console.Write(array[i, j] + " ");
+        }
+        Console.WriteLine();
+    }
+
+}
+
+massiv(m, n);
+Console.WriteLine("Исходный массив:");
+printm(array);
+
+
+void uporyadok(int[,] array)
+{
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        for (int j = 0; j < array.GetLength(1); j++)
+        {
+            for (int k = 0; k < array.GetLength(1) - 1; k++)
+            {
+                if (array[i, k] < array[i, k + 1])
+                {
+                    int temp = array[i, k + 1];
+                    array[i, k + 1] = array[i, k];
+                    array[i, k] = temp;
+                    Console.Write(array[i, k] + " ");
+
+                }
+            
+            }
+            
+        }
+   
+    }
+     Console.WriteLine();
+}
+
+uporyadok(array);
+Console.WriteLine("Отсортированный массив:");
+printm(array);
