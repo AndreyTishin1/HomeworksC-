125523b69d1fc66f43d22311fd0d27fa6b8ad824
Andrey <atishin1999@mail.ru>
2023-07-29 09:50:11 +0300

    zadacha66

---
+++ homework9/zadacha66/Program.cs
@@ -0,0 +1,11 @@
+﻿Console.Write("Введите значение M:" );
+int m = int.Parse( Console.ReadLine() );
+Console.Write("Введите значение N:");
+int n = int.Parse( Console.ReadLine() );
+int summa(int m, int n)
+{
+    if (m == n)
+        return n;
+    else return m + summa(m + 1, n);
+}
+Console.Write($"Сумма натуральных элементов в промежутке от M до N равно { summa(m, n)}");
